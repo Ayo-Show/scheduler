b58b350944185cc76d930d812b5d2a309120f6b6
Ayo-Show <sowunmi30@gmail.com>
2021-12-14 17:03:46 -0500

    Added a save funtion to app and a PUT request to update database

--- src/components/Application.js
+++ src/components/Application.js
@@ -19,11 +19,30 @@ export default function Application(props) {
     interviewers: {},
   });
 
-  const interviewers = getInterviewersForDay(state, state.day);
+  //Updating state object starting at the lowest level
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios.put(`api/appointments/${id}`, appointment).then(() => {
+      setState({ ...state, appointments });
+    });
+  }
+
   const appointments = getAppointmentsForDay(state, state.day);
 
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
+    const interviewers = getInterviewersForDay(state, state.day);
 
     return (
       <Appointment
@@ -32,6 +51,7 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
       />
     );
   });
@@ -72,7 +92,10 @@ export default function Application(props) {
         />
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
-      <section className="schedule">{schedule}</section>
+      <section className="schedule">
+        {schedule}
+        <Appointment key="last" time="5pm" />
+      </section>
     </main>
   );
 }
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,15 +6,30 @@ import Empty from "components/Appointment/Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
 
-export default function Appointment(props) {
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+
+export default function Appointment(props) {
+  
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    props.bookInterview(props.id, interview).then(() => {
+      transition(SHOW);
+    });
+  }
+
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -26,7 +41,13 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onCancel={() => back(EMPTY)}
+          onSave={save}
+        />
+      )}
     </article>
   );
 }
